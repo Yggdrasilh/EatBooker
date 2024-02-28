a9cd199cb4e211bb2d4e4df7a9d4235f9a84bb5b
JulieLenoir <renoujulie1@gmail.com>
2024-02-28 15:57:21 +0100

    header logo et boutons connexion user et restaurant - css header et @mediaqueries 480px #26

--- Projet_EatBooker/Structure_MVC/Views/base.php
+++ Projet_EatBooker/Structure_MVC/Views/base.php
@@ -5,45 +5,106 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
+    <!-- lien bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+    <!-- lien feuille de style -->
     <link rel="stylesheet" href="style.css">
+
+    <!--  liens font awesome et google fonts -->
     <script src="https://kit.fontawesome.com/cff33ecd93.js" crossorigin="anonymous"></script>
+    <script src="https://fonts.google.com/specimen/Libre+Baskerville?query=libre"></script>
 </head>
 
 <body>
-    <div id="wrapper" style="margin: 0 auto; width:95%;">
-        <header class="row">
-            <h1 class="text-center cols-12">Mon Portfolio</h1>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary cols-12">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Mon portfolio</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/home">Accueil</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php?controller=Creation&action=index">Mes création</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php?controller=User&action=monCompte">Mon compte</a>
-                            </li>
-                        </ul>
-                    </div>
+    <div id="wrapper">
+
+        <!---------- STRUCTURE HEADER --------------------- -->
+        <header id="header_sans_nav" class="row">
+
+
+            <div id="header_logo_connexion">
+
+
+                <!-- logo ATTENTION MODIFIER LE LIEN ajout lien vers accueil-->
+                <a href="#"><img src="../public/images/logoSansFond.png" alt="logo eat booker" id="logo_header"></a>
+
+                <div id="header_connexion">
+
+                    <!--bouton connexion restaurateur ATTENTION CHANGER LE LIEN mettre lien vers formulaire de connexion restaurant-->
+                    <a href="#" id="connexion_restaurant">
+                        <button type="button" class="btn btn-light connexion_button" id="connexion_restaurant">
+                            <p class="text_button">J'ai un restaurant</p>
+                        </button>
+                    </a>
+
+
+                    <!--bouton inscription/connexion user et admin ATTENTION CHANGER LE LIEN mettre lien vers formulaire de connexion -->
+                    <a href="#" id="connexion_user">
+                        <button type="button" class="btn btn-light connexion_button" id="connexion_user">
+                            <p class="text_button">Connexion/Inscription</p>
+                        </button>
+                    </a>
                 </div>
-            </nav>
+
+            </div>
+
+
+
         </header>
+        <!-- ------------FIN HEADER---------------------- -->
+
+
+
+        <!-- NAVIGATION CONDITIONNEE EN FONCTION DU ROLE - VOIR nav.php pour la structure -->
+        <?php
+        $navigation = isset($_SESSION['id_user']) ? $nav : '';
+
+        echo $navigation;
+        ?>
+
+
+        <!-- menu burger pour mobile media queries 480px -->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="menu-icon">
+            <path fill="#313131" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+        </svg>
+
+
+        <!-- ---------------------------------FIN NAVIGATION--------------------------------------------------------- -->
+
+
+
         <main>
             <?= $content ?>
         </main>
-        <footer>
-            <p class="text-center">Mon portfolio Copyright 2023</p>
+
+
+
+
+        <!---------- STRUCTURE FOOTER --------------------- -->
+        <footer id="footer">
+            <p class="text-center">EatBooker Copyright 2024</p>
         </footer>
+
+
+        <!---------- FIN FOOTER --------------------- -->
+
+
+
+
+
+
+
     </div>
+    <!-- ----- FIN DE WRAPPER------------ -->
+
+
+    <!-- CONNEXION FICHIER SCRIPT.JS DANS DOSSIER JS -->
+    <script src="js/scripts.js"></script>
+
+    <!-- CONNEXION js bootstrap -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
