7e4d691acba704311100599bf58d798393a1871f
Herminet Simon <simon.herminet@gmail.com>
2024-03-01 14:53:19 +0100

    nav debuté mais pas fini :)

--- Projet_EatBooker/Structure_MVC/Views/nav.php
+++ Projet_EatBooker/Structure_MVC/Views/nav.php
@@ -5,7 +5,7 @@ function generateNavigation($role)
     switch ($role) {
         case 'user':
             $nav_elements = array(
-                '<a class="nav" href="#"><button type="button" class="btn btn-light btnNav">Accueil</button></a>',
+                '<a class="nav" href="http://localhost:8888/Projet_EatBooker/Projet_EatBooker/Structure_MVC/public/"><button type="button" class="btn btn-light btnNav">Accueil</button></a>',
                 '<a class="nav" href="#"><button type="button" class="btn btn-light btnNav">Page profil</button></a>',
                 '<a class="nav" href="#"><button type="button" class="btn btn-light btnNav">Trouver un restaurant</button></a>',
                 '<a class="nav" href="#"><button type="button" class="btn btn-light btnNav">Mes restaurants préférés</button></a>',
@@ -21,7 +21,7 @@ function generateNavigation($role)
             break;
         case 'restaurant':
             $nav_elements = array(
-                '<a class="nav" href="#"> <button type="button" class="btn btn-light btnNav"> Accueil </button> </a>',
+                '<a class="nav" href="http://localhost:8888/Projet_EatBooker/Projet_EatBooker/Structure_MVC/public/"> <button type="button" class="btn btn-light btnNav"> Accueil </button> </a>',
                 '<a class="nav" href="#"> <button type="button" class="btn btn-light btnNav"> Page du Restaurant </button> </a>',
                 '<a class="nav" href="#"> <button type="button" class="btn btn-light btnNav"> Planning du Restaurant </button> </a>',
                 '<a class="nav" href="#"> <button type="button" class="btn btn-light btnNav"> Gerer les demandes de reservation </button> </a>',
