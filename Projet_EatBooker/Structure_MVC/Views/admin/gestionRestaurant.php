3ffc81fa8c848f17fcbf79a56bf6cfbf56df68af
Noah <noah.pouplin@gmail.com>
2024-03-04 15:34:22 +0100

    Affichage des tableau (liste resto)(resto a valider) avec update, delete et validation #19

--- Projet_EatBooker/Structure_MVC/Views/admin/gestionRestaurant.php
+++ Projet_EatBooker/Structure_MVC/Views/admin/gestionRestaurant.php
@@ -9,6 +9,7 @@ $title = "Gestion des restaurants - Administrateur ";
         <tr>
             <th>Nom</th>
             <th>Email</th>
+            <th>Mdp</th>
             <th>Adresse</th>
             <th>cp</th>
             <th>ville</th>
@@ -22,24 +23,25 @@ $title = "Gestion des restaurants - Administrateur ";
             <th>Delete</th>
         </tr>
     </thead>
-    <tbody>
+    <tbody id="bodyTableauRestoValid">
         <?php
         foreach ($restoValid as $value) {
         ?>
-            <tr class="rowRestoValid" id="<?= $value['id_restaurant'] ?>" data-idPlanning="<?= $value['id_planning'] ?>">
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['nom_restaurant'] ?>" id="nom_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['email_restaurant'] ?>" id="email_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['adresse_restaurant'] ?>" id="adresse_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['cp_restaurant'] ?>" id="cp_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['ville_restaurant'] ?>" id="ville_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['description_restaurant'] ?>" id="description_restaurant"></td>
-                <td><input type="number" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['place_max_restaurant'] ?>" id="place_max_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['prix_restaurant'] ?>" id="prix_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['menu_restaurant'] ?>" id="menu_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['type_restaurant'] ?>" id="type_restaurant"></td>
-                <td><input type="number" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['note_moyenne_restaurant'] ?>" id="note_moyenne_restaurant"></td>
-                <td> <button class="btn btn-outline-primary modifier" id="modif<?= $value['id_restaurant'] ?>">Update</button></td>
-                <td> <button class="btn btn-outline-danger supprimer" id="supp<?= $value['id_restaurant'] ?>">Delete</button></td>
+            <tr class="rowRestoValid" id="rowRestoValid<?= $value['id_restaurant'] ?>">
+                <td><input type="text" class="form-control input" value="<?= $value['nom_restaurant'] ?>" id="nom_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['email_restaurant'] ?>" id="email_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['password_restaurant'] ?>" id="password_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['adresse_restaurant'] ?>" id="adresse_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['cp_restaurant'] ?>" id="cp_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['ville_restaurant'] ?>" id="ville_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['description_restaurant'] ?>" id="description_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="number" class="form-control input" value="<?= $value['place_max_restaurant'] ?>" id="place_max_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['prix_restaurant'] ?>" id="prix_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['menu_restaurant'] ?>" id="menu_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['type_restaurant'] ?>" id="type_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="number" class="form-control input" value="<?= $value['note_moyenne_restaurant'] ?>" id="note_moyenne_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td> <button class="btn btn-outline-primary modifier" id="<?= $value['id_restaurant'] ?>" data-idPlanning="<?= $value['id_planning'] ?>" data-statut="<?= $value['statut_restaurant'] ?>">Update</button></td>
+                <td> <button class="btn btn-outline-danger supprimer" id="<?= $value['id_restaurant'] ?>" data-statut="<?= $value['statut_restaurant'] ?>">Delete</button></td>
             </tr>
         <?php
         }
@@ -54,6 +56,7 @@ $title = "Gestion des restaurants - Administrateur ";
         <tr>
             <th>Nom</th>
             <th>Email</th>
+            <th>Mdp</th>
             <th>Adresse</th>
             <th>cp</th>
             <th>ville</th>
@@ -72,21 +75,22 @@ $title = "Gestion des restaurants - Administrateur ";
         <?php
         foreach ($restoAValider as $value) {
         ?>
-            <tr class="rowRestoAValider" id="<?= $value['id_restaurant'] ?>" data-idPlanning="<?= $value['id_planning'] ?>">
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['nom_restaurant'] ?>" id="nom_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['email_restaurant'] ?>" id="email_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['adresse_restaurant'] ?>" id="adresse_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['cp_restaurant'] ?>" id="cp_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['ville_restaurant'] ?>" id="ville_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['description_restaurant'] ?>" id="description_restaurant"></td>
-                <td><input type="number" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['place_max_restaurant'] ?>" id="place_max_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['prix_restaurant'] ?>" id="prix_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['menu_restaurant'] ?>" id="menu_restaurant"></td>
-                <td><input type="text" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['type_restaurant'] ?>" id="type_restaurant"></td>
-                <td><input type="number" class="form-control input<?= $value['id_restaurant'] ?>" value="<?= $value['note_moyenne_restaurant'] ?>" id="note_moyenne_restaurant"></td>
-                <td><button class="btn btn-outline-success valider" id="<?= $value['id_restaurant'] ?>">Valider</button></td>
-                <td><button class="btn btn-outline-primary modifier" id="<?= $value['id_restaurant'] ?>">Update</button></td>
-                <td><button class="btn btn-outline-danger supprimer" id="<?= $value['id_restaurant'] ?>">Delete</button></td>
+            <tr class="rowRestoAValider" id="rowRestoAValider<?= $value['id_restaurant'] ?>">
+                <td><input type="text" class="form-control input" value="<?= $value['nom_restaurant'] ?>" id="nom_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['email_restaurant'] ?>" id="email_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['password_restaurant'] ?>" id="password_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['adresse_restaurant'] ?>" id="adresse_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['cp_restaurant'] ?>" id="cp_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['ville_restaurant'] ?>" id="ville_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['description_restaurant'] ?>" id="description_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="number" class="form-control input" value="<?= $value['place_max_restaurant'] ?>" id="place_max_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['prix_restaurant'] ?>" id="prix_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['menu_restaurant'] ?>" id="menu_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="text" class="form-control input" value="<?= $value['type_restaurant'] ?>" id="type_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><input type="number" class="form-control input" value="<?= $value['note_moyenne_restaurant'] ?>" id="note_moyenne_restaurant<?= $value['id_restaurant'] ?>"></td>
+                <td><button class="btn btn-outline-success valider" id="<?= $value['id_restaurant'] ?>" data-idPlanning="<?= $value['id_planning'] ?>">Valider</button></td>
+                <td><button class="btn btn-outline-primary modifier" id="<?= $value['id_restaurant'] ?>" data-idPlanning="<?= $value['id_planning'] ?>" data-statut="<?= $value['statut_restaurant'] ?>">Update</button></td>
+                <td><button class="btn btn-outline-danger supprimer" id="<?= $value['id_restaurant'] ?>" data-statut="<?= $value['statut_restaurant'] ?>">Delete</button></td>
             </tr>
         <?php
         }
