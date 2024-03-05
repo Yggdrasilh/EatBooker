6975336d2a72edefd5ce41942b73e355c3e72f0c
Noah <noah.pouplin@gmail.com>
2024-03-05 15:34:44 +0100

    insciption resto et création planning #19

--- Projet_EatBooker/Structure_MVC/Controllers/RestaurantController.php
+++ Projet_EatBooker/Structure_MVC/Controllers/RestaurantController.php
@@ -71,9 +71,158 @@ class RestaurantController extends Controller
 
     public function inscriptionRestaurant()
     {
-        if (Validator::validPostGlobal()) {
+        if (empty($_POST)) {
+            $this->render('restaurant/inscription', ['erreur' => false]);
         } else {
-            $this->render('restaurant/inscription', ['renseignerPlanning' => false]);
+
+            if (Validator::validPostGlobal()) {
+                //Création du body et récupération de toute les data a envoyer dans la requête
+                $tableau = [
+                    'http' => [
+                        'method' => "POST",
+                        'header' => ["Content-type: application/json"],
+                        'content' => json_encode([
+                            'nom_restaurant' => $_POST['inputeNom'],
+                            'email_restaurant' => $_POST['inputeEmail'],
+                            'password_restaurant' => $_POST['inputePassword'],
+                            'adresse_restaurant' => $_POST['inputeAdresse'],
+                            'cp_restaurant' => $_POST['inputeCp'],
+                            'ville_restaurant' => $_POST['inputeVille'],
+                            'description_restaurant' => $_POST['inputeDescription'],
+                            'place_max_restaurant' => $_POST['inputeNbCouvertMax'],
+                            'prix_restaurant' => $_POST['inputePrixMoyen'],
+                            'menu_restaurant' => $_POST['inputeMenu'],
+                            'type_restaurant' => $_POST['inputeTypeCuisine'],
+                        ]),
+                    ]
+                ];
+                $tab = stream_context_create($tableau);
+
+                //envoie de la requête
+                $data = file_get_contents($this->baseUrlApi . '/restaurant/add', false, $tab);
+                $convert = json_decode($data, true);
+
+                //Gestion des erreurs
+                if ($convert['status']) {
+                    header("Location:" . $this->baseUrlSite . "/");
+                } else {
+                    $this->render('restaurant/inscription', ['erreur' => true]);
+                }
+            } else {
+                $this->render('restaurant/inscription', ['erreur' => true]);
+            }
+        }
+    }
+
+
+    public function planningRestaurant()
+    {
+        if (empty($_POST)) {
+            $this->render('restaurant/formPlanning', ['erreur' => false]);
+        } else {
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+            // Mise a jour  de la clé de l'id_planning dans la table restaurant
+
+            $dataResto = file_get_contents($this->baseUrlApi . '/restaurant/find/28'); // remplacer 28 par  l'id du restaurant connecté => $_SESSION['?']
+            $resto = json_decode($dataResto, true);
+
+            $tableauResto = [
+                'http' => [
+                    'method' => "POST",
+                    'header' => ["Content-type: application/json"],
+                    'content' => json_encode([
+                        'nom_restaurant' => $resto['restaurant']['nom_restaurant'],
+                        'email_restaurant' => $resto['restaurant']['email_restaurant'],
+                        'password_restaurant' => $resto['restaurant']['password_restaurant'],
+                        'adresse_restaurant' => $resto['restaurant']['adresse_restaurant'],
+                        'cp_restaurant' => $resto['restaurant']['cp_restaurant'],
+                        'ville_restaurant' => $resto['restaurant']['ville_restaurant'],
+                        'description_restaurant' => $resto['restaurant']['description_restaurant'],
+                        'place_max_restaurant' => $resto['restaurant']['place_max_restaurant'],
+                        'prix_restaurant' => $resto['restaurant']['prix_restaurant'],
+                        'menu_restaurant' => $resto['restaurant']['menu_restaurant'],
+                        'type_restaurant' => $resto['restaurant']['type_restaurant'],
+                        'note_moyenne_restaurant' => $resto['restaurant']['note_moyenne_restaurant'],
+                        'statut_restaurant' => $resto['restaurant']['statut_restaurant'],
+                        'id_planning' => $resto['restaurant']['id_restaurant'],
+                    ]),
+                ]
+            ];
+
+            $tabResto = stream_context_create($tableauResto);
+
+            //envoie de la requête
+            $dataResultResto = file_get_contents($this->baseUrlApi . '/restaurant/update/28', false, $tabResto); // remplacer 28 par  l'id du restaurant connecté => $_SESSION['?']
+            $convertResultResto = json_decode($dataResultResto, true);
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            if ($convertResultResto['status']) {
+
+
+                //eviter les valeur null et attribuer les false au champs non cocher
+                $post = [
+                    'checkLundiAm' => false,
+                    'checkLundiPm' => false,
+                    'checkMardiAm' => false,
+                    'checkMardiPm' => false,
+                    'checkMercrediAm' => false,
+                    'checkMercrediPm' => false,
+                    'checkJeudiAm' => false,
+                    'checkJeudiPm' => false,
+                    'checkVendrediAm' => false,
+                    'checkVendrediPm' => false,
+                    'checkSamediAm' => false,
+                    'checkSamediPm' => false,
+                    'checkDimancheAm' => false,
+                    'checkDimanchePm' => false,
+                ];
+                foreach ($_POST as $key => $value) {
+                    if (!empty($value) && isset($value)) {
+                        $post[$key] = true;
+                    }
+                }
+
+                //Création du body et récupération de toute les data a envoyer dans la requête
+                $tableau = [
+                    'http' => [
+                        'method' => "POST",
+                        'header' => ["Content-type: application/json"],
+                        'content' => json_encode([
+                            'id_planning' => 28, // l'id du resto puisque il ya un planning par resto  =>  $_SESSION['idResto ?']
+                            'lundi_am' => $post['checkLundiAm'],
+                            'lundi_pm' => $post['checkLundiPm'],
+                            'mardi_am' => $post['checkMardiAm'],
+                            'mardi_pm' => $post['checkMardiPm'],
+                            'mercredi_am' => $post['checkMercrediAm'],
+                            'mercredi_pm' => $post['checkMercrediPm'],
+                            'jeudi_am' => $post['checkJeudiAm'],
+                            'jeudi_pm' => $post['checkJeudiPm'],
+                            'vendredi_am' => $post['checkVendrediAm'],
+                            'vendredi_pm' => $post['checkVendrediPm'],
+                            'samedi_am' => $post['checkSamediAm'],
+                            'samedi_pm' => $post['checkSamediPm'],
+                            'dimanche_am' => $post['checkDimancheAm'],
+                            'dimanche_pm' => $post['checkDimanchePm'],
+                            'id_restaurant' => 28, //definir le nom de la session pour l'id des restaurant  => $_SESSION['?']
+                        ]),
+                    ]
+                ];
+                $tab = stream_context_create($tableau);
+
+                //envoie de la requête
+                $data = file_get_contents($this->baseUrlApi . '/planning/add', false, $tab);
+                $convert = json_decode($data, true);
+                var_dump($data);
+                //Gestion des erreurs
+                if ($convert['status']) {
+                    header("Location:" . $this->baseUrlSite . "/");
+                } else {
+                    $this->render('restaurant/formPlanning', ['erreur' => true]);
+                }
+            } else {
+                $this->render('restaurant/formPlanning', ['erreur' => true]);
+            }
         }
     }
 }
--- Projet_EatBooker/Structure_MVC/Views/restaurant/inscription.php
+++ Projet_EatBooker/Structure_MVC/Views/restaurant/inscription.php
@@ -1,158 +1,76 @@
 <?php
 
 $title = "Inscription restaurateur ";
-$keysResto = [
-    'nom_restaurant',
-    'email_restaurant',
-    'password_restaurant',
-    'adresse_restaurant',
-    'cp_restaurant',
-    'ville_restaurant',
-    'description_restaurant',
-    'place_max_restaurant',
-    'prix_restaurant',
-    'menu_restaurant',
-    'type_restaurant',
-    'note_moyenne_restaurant',
-    'statut_restaurant',
-    'id_planning',
-];
 
-$keysPlanning = [
-    'lundi_am',
-    'lundi_pm',
-    'mardi_am',
-    'mardi_pm',
-    'mercredi_am',
-    'mercredi_pm',
-    'jeudi_am',
-    'jeudi_pm',
-    'vendredi_am',
-    'vendredi_pm',
-    'samedi_am',
-    'samedi_pm',
-    'dimanche_am',
-    'dimanche_pm',
-    'id_restaurant',
-];
+if ($erreur) {
+?>
+    <div class="alert alert-danger" role="alert">
+        Un problème est survenue lors de votre inscription. Veuillez réessayer.
+    </div>
+<?php
+}
 ?>
 
-<form action="" method="post" id="inscriptionRestaurant">
-
-    <?php
-    if ($renseignerPlanning) {
-    ?>
-        <h2>Rentrer les informations de votre restaurant</h2>
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Nom</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-text" id="basic-addon2">Email</span>
-        </div>
-
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Password</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-text" id="basic-addon2">Adresse</span>
-        </div>
-
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">CP</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-text" id="basic-addon2">Ville</span>
-        </div>
-
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Description</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-text" id="basic-addon2">Nombre de couvert maximum</span>
-        </div>
-
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Prix moyen d'un menue (fourchette de prix)</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
-            <span class="input-group-text" id="basic-addon2">Menu (URL du fichier PDF ou image)</span>
-        </div>
-
-        <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Type de cuisine</span>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
-
-    <?php
-    } else {
-    ?>
+<form action="http://application/Projet_EatBooker_2s/Projet_EatBooker/Structure_MVC/public/index.php?controller=Restaurant&action=inscriptionRestaurant" method="post" id="inscriptionRestaurant">
 
 
+    <h2>Rentrer les informations de votre restaurant</h2>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="spanNom">Nom</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="inpute" id="inputeNom" name="inputeNom">
+    </div>
 
+    <div class="input-group mb-3">
+        <input type="email" class="form-control" placeholder="truc@mail.co" aria-label="Recipient's username" aria-describedby="inpute" id="inputeEmail" name="inputeEmail">
+        <span class="input-group-text" id="spanEmail">Email</span>
+    </div>
 
-        <h2>Rentrer vos horaire d'ouverture</h2>
-        <table class="table table-striped table-dark">
-            <thead>
-                <tr>
-                    <th>Jours</th>
-                    <th>AM</th>
-                    <th>PM</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th>Lundi</th>
-                    <td> <input class="form-check-input" type="checkbox" id="checkLundiAm" name="checkLundiAm"></td>
-                    <td> <input class="form-check-input" type="checkbox" id="checkLundiPm" name="checkLundiPm"></td>
-                </tr>
-                <tr>
-                    <th>Mardi</th>
-                    <td> <input class="form-check-input" type="checkbox" id="checkMardiAm" name="checkMardiAm"></td>
-                    <td> <input class="form-check-input" type="checkbox" id="checkMardiPm" name="checkMardiPm"></td>
-                </tr>
-                <tr>
-                    <th>Mercredi</th>
-                    <td> <input class="form-check-input" type="checkbox" id="checkMercrediAm" name="checkMercrediAm"></td>
-                    <td> <input class="form-check-input" type="checkbox" id="checkMercrediPm" name="checkMercrediPm"></td>
-                </tr>
-                <tr>
-                    <th>Jeudi</th>
-                    <td> <input class="form-check-input" type="checkbox" id="checkJeudiAm" name="checkJeudiAm"></td>
-                    <td> <input class="form-check-input" type="checkbox" id="checkJeudiPm" name="checkJeudiPm"></td>
-                </tr>
-                <tr>
-                    <th>Vendredi</th>
-                    <td> <input class="form-check-input" type="checkbox" id="checkVendrediAm" name="checkVendrediAm"></td>
-                    <td> <input class="form-check-input" type="checkbox" id="checkVendrediPm" name="checkVendrediPm"></td>
-                </tr>
-                <tr>
-                    <th>Samedi</th>
-                    <td> <input class="form-check-input" type="checkbox" id="checkSamediAm" name="checkSamediAm"></td>
-                    <td> <input class="form-check-input" type="checkbox" id="checkSamediPm" name="checkSamediPm"></td>
-                </tr>
-                <tr>
-                    <th>Dimanche</th>
-                    <td> <input class="form-check-input" type="checkbox" id="checkDimancheAm" name="checkDimancheAm"></td>
-                    <td> <input class="form-check-input" type="checkbox" id="checkDimanchePm" name="checkDimanchePm"></td>
-                </tr>
-            </tbody>
-        </table>
-    <?php
-    }
-    ?>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="spanPassword">Password</span>
+        <input type="password" class="form-control" placeholder="123456" aria-label="Username" aria-describedby="inpute" id="inputePassword" name="inputePassword">
+    </div>
+
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="10 allée de truc" aria-label="Recipient's username" aria-describedby="inpute" id="inputeAdresse" name="inputeAdresse">
+        <span class="input-group-text" id="spanAdresse">Adresse</span>
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="spanCp">CP</span>
+        <input type="number" class="form-control" placeholder="49000" aria-label="Username" aria-describedby="inpute" id="inputeCp" name="inputeCp">
+    </div>
+
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Angers" aria-label="Recipient's username" aria-describedby="inpute" id="inputeVille" name="inputeVille">
+        <span class="input-group-text" id="spanVille">Ville</span>
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="spanDescription">Description</span>
+        <input type="text" class="form-control" placeholder="blablabla" aria-label="Username" aria-describedby="inpute" id="inputeDescription" name="inputeDescription">
+    </div>
+
+    <div class="input-group mb-3">
+        <input type="number" class="form-control" placeholder="30" aria-label="Recipient's username" aria-describedby="inpute" id="inputeNbCouvertMax" name="inputeNbCouvertMax">
+        <span class="input-group-text" id="spanNbCouvertMax">Nombre de couvert maximum</span>
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="spanPrixMoyen">Prix moyen d'un menue (fourchette de prix)</span>
+        <input type="text" class="form-control" placeholder="30€ à 60€" aria-label="Username" aria-describedby="inpute" id="inputePrixMoyen" name="inputePrixMoyen">
+    </div>
+
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="https://pdf/resto..." value="Menue" aria-label="Recipient's username" aria-describedby="inpute" id="inputeMenu" name="inputeMenu">
+        <span class="input-group-text" id="spanMenu">Menu (URL du fichier PDF ou image)</span>
+    </div>
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="spanTypeCuisine">Type de cuisine</span>
+        <input type="text" class="form-control" placeholder="Bistro" aria-label="Username" aria-describedby="inpute" id="inputeTypeCuisine" name="inputeTypeCuisine">
+    </div>
+
+    <div class="col-12 submitFormResto">
+        <button class="btn btn-primary" type="submit">Envoyer</button>
+    </div>
 </form>
