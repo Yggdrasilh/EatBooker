f43c45544d07f37b06d534ee87274534d3602267
JulieLenoir <renoujulie1@gmail.com>
2024-03-05 13:38:56 +0100

    ne pas prendre en compte #12

--- Projet_EatBooker/Structure_MVC/Views/user/favori.php
+++ Projet_EatBooker/Structure_MVC/Views/user/favori.php
@@ -6,3 +6,19 @@ echo "Bonjour " . $_SESSION['prenom_user'];
 
 ?>
 <h1>Mes restaurants Favoris</h1>
+
+<?php
+foreach ($favoriData as $favori) : ?>
+    <div class="card mes_favoris" style="width: 18rem;">
+        <div class="card-body">
+            <h4 class="card-title favori"><b> Nom du Restaurant :</b> <?= $favori['nom_restaurant'] ?></h4>
+            <h4 class="card-title favori"><b>Ville :</b> <?= $favori['ville_restaurant']  ?></h4>
+            <a href="index.php?controller=restaurant&action=find&id=<?= $favori['id_restaurant']  ?>">
+                <p class="card-text">Voir la fiche du restaurant</p>
+            </a>
+            <a href="index.php?controller=restaurant&action=deleteFavori&id=<?= $favori['id_restaurant']  ?>">
+                <p class="card-text">Supprimer ce restaurant des favoris</p>
+            </a>
+        </div>
+    </div>
+<?php endforeach ?>
--- Projet_EatBooker/Structure_MVC/Views/user/profil.php
+++ Projet_EatBooker/Structure_MVC/Views/user/profil.php
@@ -17,19 +17,5 @@
         </div>
     </div>
 
-    <?php
-    foreach ($favoriData as $favori) : ?>
-        <div class="card mes_favoris" style="width: 18rem;">
-            <div class="card-body">
-                <h4 class="card-title favori"><b> Nom du Restaurant :</b> <?= $favori['nom_restaurant'] ?></h4>
-                <h4 class="card-title favori"><b>Ville :</b> <?= $favori['ville_restaurant']  ?></h4>
-                <a href="index.php?controller=restaurant&action=find&id=<?= $favori['id_restaurant']  ?>">
-                    <p class="card-text">Voir la fiche du restaurant</p>
-                </a>
-                <a href="index.php?controller=restaurant&action=deleteFavori&id=<?= $favori['id_restaurant']  ?>">
-                    <p class="card-text">Supprimer ce restaurant des favoris</p>
-                </a>
-            </div>
-        </div>
-    <?php endforeach ?>
+
 </section>
